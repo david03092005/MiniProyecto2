05faf17b2d2b2effb8c977e2e3c78eeecc833ff6
david03092005 <dasalazarzuleta@javerianacali.edu.co>
2024-09-05 03:01:16 -0500

    History, json file implementation, and functionality improvements

--- graph.py
+++ graph.py
@@ -1,29 +1,36 @@
 import os
 import openal
+import sys
+import time
 
 class node:
-    def __init__(self, num, sound, gain = 1, text=""):
-        self.__num = num
+    def __init__(self, id, sound, position, gain = 1, text="", option=""):
+        self.__id = id
         self.__sound = sound
-        self.__text = text
+        self.__position = position
         self.__gain = gain
+        self.__text = text
+        self.__option = option
 
     def get_num(self):
-        return self.__num
+        return self.__id
     
     def get_text(self):
         return self.__text
+
+    def get_option(self):
+        return self.__option
     
     def play(self):
         currentDir = os.path.dirname(os.path.abspath(__file__))
         fileM = os.path.join(currentDir, "Music", self.__sound)
-        openal.oalInit()
         source = openal.oalOpen(fileM)
         source.set_gain(self.__gain)
+        source.set_position(self.__position)
         source.play()
         while source.get_state() == openal.AL_PLAYING:
             pass
-        openal.oalQuit()
+        
 
 
 class graph:
@@ -42,8 +49,8 @@ class graph:
     def take_edge(self, posEdge):
         return self.__graph[posEdge]
     
-    def get_node(self, node:node):
+    def get_node(self, nodeID):
         for key_node in self.__graph.keys():
-            if key_node == node:
+            if key_node.get_num() == nodeID:
                 return key_node
         return None
--- main.py
+++ main.py
@@ -1,35 +1,79 @@
 from graph import graph, node
+import openal
+import sys
+import time
+import threading
+import json
+import os
+
 
 def main():
+    global g
+    openal.oalInit()
     # Create a new graph
     g = graph()
-    nodeList = [    
-        node(1, "sil.wav", 0.5, "Eres un cientifico que descubrio como viajar en el tiempo, te entro una gran curiosidad por lo que pasaria si cambias algo del pasado, y puedes viajar a estas epocas: "), 
-        node(2, "time.wav", 1, "Epoca medieval"), 
-        node(3, "disparos.wav", 1, "Epoca del antiguo egipto")
-    ]
-    for i in nodeList:
-        g.new_node(i)
-
-    nodeSound = g.get_node(nodeList[0])
-    print(nodeSound.get_text())
-    nodeSound.play()
-
-    g.new_edge((nodeList[0], nodeList[1]))
-    g.new_edge((nodeList[0], nodeList[2]))
-
-
-    currentNode = g.get_node(nodeList[0])
-    finishNode = g.get_node(nodeList[-1])
-    while (currentNode != finishNode):
-        print(currentNode.get_text())
-        paths = g.get_edge_node(currentNode)
-        print("Puedes hacer lo siguiente: ")
-        for i, n in enumerate(paths):
-            print(i+1, "-->", n.get_text())
     
-        next = input("Que deseas Hacer: ")
+    data = loadFromJson("info.json")
+    # nodeList = processNodes(data)
+    processNodes(data)
 
+    paths = loadFromJson("paths.json")
+    processEdges(paths)
+
+    currentNode = g.get_node(0)
+    finishNode = g.get_node(3)
+    while (currentNode != finishNode):
+        textThread = threading.Thread(target=writeMachine, args=(currentNode.get_text(), 0.01))
+        musicThread = threading.Thread(target=currentNode.play)
+        musicThread.start()
+        textThread.start()
+        musicThread.join()
+        textThread.join()
+        paths = g.get_edge_node(currentNode)
+        for i, n in enumerate(paths):
+            text = str(i+1) + " --> " + n.get_option()
+            writeMachine(text, 0.005)
+    
+        writeMachine("Que deseas Hacer: ", 0.003)
+        next = int(input())
+        currentNode = paths[next - 1]
+
+    writeMachine(currentNode.get_text(), 0.01)
+    currentNode.play()
+    writeMachine("El juego a terminado, Muchas gracias por jugar, vuelve cuando quieras.", 0.06)
+
+    openal.oalQuit()
+
+
+def writeMachine(text, delay):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()
+
+
+def loadFromJson(name):
+    currentDir = os.path.dirname(os.path.abspath(__file__))
+    fileName = os.path.join(currentDir, "infoHistory", name)
+    with open(fileName, 'r') as file:
+        data = json.load(file)
+    return data
+
+
+def processNodes(data):
+    global g
+    for n in data:
+        g.new_node(node(n['id'], n['sound'], list(n['position']), n['volume'], n['text'], n['option']))
+
+
+def processEdges(data):
+    global g
+    for edge in data:
+        nodeOut = g.get_node(edge['from'])
+        for inEdge in edge['path']:
+            nodeIn = g.get_node(inEdge)
+            g.new_edge((nodeOut, nodeIn))
 
 
 main()
