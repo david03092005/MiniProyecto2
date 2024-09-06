dd6c15f22a8d0e920d6edd62e091fe72f6eb5b5b
david03092005 <dasalazarzuleta@javerianacali.edu.co>
2024-09-05 19:43:18 -0500

    Game update (Egypt)

--- graph.py
+++ graph.py
@@ -3,14 +3,19 @@ import openal
 import sys
 import time
 
+
+sys.stdout.reconfigure(encoding='utf-8')
+
 class node:
-    def __init__(self, id, sound, position, gain = 1, text="", option=""):
+    def __init__(self, id, sounds, positions, gain = 1, text="", option="", final=False):
         self.__id = id
-        self.__sound = sound
-        self.__position = position
+        self.__sounds = sounds
+        self.__positions = positions
         self.__gain = gain
         self.__text = text
         self.__option = option
+        self.__final = final
+        self.__stop = False
 
     def get_num(self):
         return self.__id
@@ -21,16 +26,38 @@ class node:
     def get_option(self):
         return self.__option
     
+    def get_final(self):
+        return self.__final
+
+    def set_stop(self):
+        self.__stop = True
+        
     def play(self):
         currentDir = os.path.dirname(os.path.abspath(__file__))
-        fileM = os.path.join(currentDir, "Music", self.__sound)
-        source = openal.oalOpen(fileM)
-        source.set_gain(self.__gain)
-        source.set_position(self.__position)
-        source.play()
-        while source.get_state() == openal.AL_PLAYING:
-            pass
+        sources = []
+        i = 0
+        for sound in self.__sounds:
+            fileM = os.path.join(currentDir, "Music", sound)
+            
+            source = openal.oalOpen(fileM)
+            source.set_gain(self.__gain[i])
+            source.set_position(self.__positions[i])
+            source.play()
+            
+            sources.append(source)
+            i += 1;
         
+        while any(source.get_state() == openal.AL_PLAYING for source in sources):
+            if (self.__stop):
+                n = 0
+                for source in sources:
+                    volum = round(self.__gain[n] * 1000)
+                    for i in range(volum, 0, -1):
+                        source.set_gain(i/1000)
+                        time.sleep(0.0005)
+                    n += 1
+
+                    source.stop()
 
 
 class graph:
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import threading
 import json
 import os
 
+sys.stdout.reconfigure(encoding='utf-8')
 
 def main():
     global g
@@ -14,33 +15,44 @@ def main():
     g = graph()
     
     data = loadFromJson("info.json")
-    # nodeList = processNodes(data)
     processNodes(data)
 
     paths = loadFromJson("paths.json")
     processEdges(paths)
 
     currentNode = g.get_node(0)
-    finishNode = g.get_node(3)
-    while (currentNode != finishNode):
-        textThread = threading.Thread(target=writeMachine, args=(currentNode.get_text(), 0.01))
+    while (not currentNode.get_final()):
+        textThread = threading.Thread(target=writeMachine, args=(currentNode.get_text(), 0.001))
         musicThread = threading.Thread(target=currentNode.play)
         musicThread.start()
         textThread.start()
-        musicThread.join()
         textThread.join()
         paths = g.get_edge_node(currentNode)
         for i, n in enumerate(paths):
             text = str(i+1) + " --> " + n.get_option()
-            writeMachine(text, 0.005)
+            writeMachine(text, 0.001)
     
-        writeMachine("Que deseas Hacer: ", 0.003)
-        next = int(input())
-        currentNode = paths[next - 1]
+        writeMachine("Que deseas Hacer: ", 0.001)
+        next = -1
+        error = True
+        while (error):
+            next = input()
+            try:
+                next = int(next)
+                if (next >= 1 and next <= len(paths)):
+                    error = False
+                else:
+                    raise ValueError()
+            except ValueError:
+                print("Entrada inválida. Asegúrate de ingresar un número entero entre 1 y", len(paths))
+        
+        currentNode.set_stop()
+        musicThread.join()
+        currentNode = paths[int(next) - 1]
 
-    writeMachine(currentNode.get_text(), 0.01)
+    writeMachine(currentNode.get_text(), 0.05)
     currentNode.play()
-    writeMachine("El juego a terminado, Muchas gracias por jugar, vuelve cuando quieras.", 0.06)
+    writeMachine("El juego a terminado, Muchas gracias por jugar, vuelve cuando quieras.", 0.04)
 
     openal.oalQuit()
 
@@ -56,7 +68,7 @@ def writeMachine(text, delay):
 def loadFromJson(name):
     currentDir = os.path.dirname(os.path.abspath(__file__))
     fileName = os.path.join(currentDir, "infoHistory", name)
-    with open(fileName, 'r') as file:
+    with open(fileName, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
@@ -64,7 +76,7 @@ def loadFromJson(name):
 def processNodes(data):
     global g
     for n in data:
-        g.new_node(node(n['id'], n['sound'], list(n['position']), n['volume'], n['text'], n['option']))
+        g.new_node(node(n['id'], n['sound'], list(n['position']), n['volume'], n['text'], n['option'], n['gameOver']))
 
 
 def processEdges(data):
